f3234c79f70ddac606ba81e23acdb9ea9a8c19f2
greyhoundforty <ryantiffany@fastmail.com>
2023-03-28 06:50:10 -0500

    Adding in logging at app level

--- build/main.py
+++ build/main.py
@@ -1,7 +1,8 @@
 import sys
 import json
-import base64
 import os
+import logging
+from logdna import LogDNAHandler
 import time
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_cloud_sdk_core import ApiException
@@ -20,21 +21,25 @@ refreshToken = authenticator.token_manager.request_token()['refresh_token']
 
 workspaceId = os.environ.get('WORKSPACE_ID')
 
-# def logDnaLogger():
-#     key = os.environ.get('LOGDNA_INGESTION_KEY')
-#     log = logging.getLogger('logdna')
-#     log.setLevel(logging.INFO)
+def logDnaLogger():
+    key = os.environ.get('LOGDNA_INGESTION_KEY')
+    log = logging.getLogger('logdna')
+    log.setLevel(logging.INFO)
+    appName = workspaceId + '-ce-job'
 
-#     options = {
-#         'app': 'rolling-iaas',
-#         'url': 'https://logs.us-south.logging.cloud.ibm.com/logs/ingest',
-#     }
+    options = {
+        'env': 'code-engine',
+        'level': 'info',
+        'app': appName,
+        'url': 'https://logs.us-south.logging.cloud.ibm.com/logs/ingest',
+        'log_error_response': True
+    }
 
-#     logger = LogDNAHandler(key, options)
+    logger = LogDNAHandler(key, options)
 
-#     log.addHandler(logger)
+    log.addHandler(logger)
 
-#     return log
+    return log
 
 def schematicsClient():
     schClient = SchematicsV1(authenticator=authenticator)
@@ -53,27 +58,31 @@ def getWorkspaceStatus():
 
 def deleteWorkspaceResources():
     client = schematicsClient()
+    log = logDnaLogger()
     wsDestroy = client.destroy_workspace_command(
     w_id=workspaceId,
     refresh_token=refreshToken
     ).get_result()
     
     destroyActivityId = wsDestroy.get('activityid')
+    # Next steo is to check the status of the workspace vs the status of the job that is running the destroy command
+    # status = getWorkspaceStatus()
+    # Need to verify the status names that get returned from the API. I believe these all use UPPERCASE
     while True:
         destroyStatus = client.get_job(job_id=destroyActivityId).get_result()['status']['workspace_job_status']['status_code']
         if (destroyStatus == 'job_in_progress' or destroyStatus == 'job_pending'):
-            print("Workspace destroy in progress. Checking again in 2 minutes...")
+            log.info("Workspace destroy in progress. Checking again in 2 minutes...")
             time.sleep(120)
         elif (destroyStatus == 'job_cancelled' or destroyStatus == 'job_failed'):
-            print("Workspace apply failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + destroyActivityId)
+            log.error("Workspace apply failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + destroyActivityId)
             break
         else:
-            print("Workspace apply complete. Gathering workspace outputs.")
+            log.info("Workspace apply complete. Gathering workspace outputs.")
             break
 
 def planWorkspace():
     client = schematicsClient()
-    # log = logDnaLogger()
+    log = logDnaLogger()
     wsPlan = client.plan_workspace_command(
         w_id=workspaceId,
         refresh_token=refreshToken
@@ -84,18 +93,18 @@ def planWorkspace():
     while True:
         planStatus = client.get_job(job_id=planActivityId).get_result()['status']['workspace_job_status']['status_code']
         if (planStatus == 'job_in_progress' or planStatus == 'job_pending'):
-            print("Workspace apply in progress. Checking again in 2 minutes...")
+            log.info("Workspace apply in progress. Checking again in 2 minutes...")
             time.sleep(120)
         elif (planStatus == 'job_cancelled' or planStatus == 'job_failed'):
-            print("Workspace apply failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + planActivityId)
+            log.error("Workspace apply failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + planActivityId)
             break
         else:
-            print("Workspace apply complete. Gathering workspace outputs.")
+            log.info("Workspace apply complete. Gathering workspace outputs.")
             break
 
 def applyWorkspace():
     client = schematicsClient()
-    # log = logDnaLogger()
+    log = logDnaLogger()
     wsApply = client.apply_workspace_command(
         w_id=workspaceId,
         refresh_token=refreshToken,
@@ -106,77 +115,22 @@ def applyWorkspace():
     while True:
         applyStatus = client.get_job(job_id=applyActivityId).get_result()['status']['workspace_job_status']['status_code']
         if (applyStatus == 'job_in_progress' or applyStatus == 'job_pending'):
-            print("Workspace apply in progress. Checking again in 2 minutes...")
+            log.info("Workspace apply in progress. Checking again in 2 minutes...")
             time.sleep(120)
         elif (applyStatus == 'job_cancelled' or applyStatus == 'job_failed'):
-            print("Workspace apply failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + applyActivityId)
+            log.error("Workspace apply failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + applyActivityId)
             break
         else:
-            print("Workspace apply complete. Gathering workspace outputs.")
+            log.info("Workspace apply complete. Gathering workspace outputs.")
             break
-
-def pullInstanceId():
-    client = schematicsClient()
-    wsOutputs = client.get_workspace_outputs(
-        w_id=workspaceId,
-    ).get_result()
-
-    instanceIdOutput = wsOutputs[0]['output_values'][0]['instance_id']['value']
-
-    return instanceIdOutput
-
-# def cosClient():
-#     # Constants for IBM COS values
-#     cosEndpoint = ("https://" + os.environ.get('COS_ENDPOINT'))
-#     cosApiKey = os.environ.get('COS_API_KEY')
-#     cosInstanceCrn    = os.environ.get('COS_INSTANCE_CRN')
-#     cos = ibm_boto3.resource("s3",
-#         ibm_api_key_id=cosApiKey,
-#         ibm_service_instance_id=cosInstanceCrn,
-#         config=Config(signature_version="oauth"),
-#         endpoint_url=cosEndpoint
-#     )
-#     return cos
-
-# def writeCosFile(instance, cosFile):
-#     client = cosClient()
-#     cosBucket = os.environ.get('COS_BUCKET')
-#     cosFile = cosFile
-#     cosFileContents = instance
-
-#     client.Object(cosBucket, cosFile).put(Body=cosFileContents)
-
 
 try:
     currentStatus = getWorkspaceStatus()
     print("Current workspace status: " + currentStatus)
     planWorkspace()
     applyWorkspace()
-    # if (currentStatus == 'INACTIVE'):
-    #     print("Workspace is INACTIVE state, no resources need to be destroyed.")
-    #     print("Running plan to deploy new resources.")
-    #     print("I would run planWorkspace() here")
-    #     print("Workspace plan complete. Running apply to deploy new resources.")
-    #     print("I would run applyWorkspace() here")
-    #     print("Workspace apply complete. New resources deployed.")
-    # else:
-    #     print("Workspace is in ACTIVE state, running destroy command to remove resources.")
-    #     print("I would run deleteWorkspaceResources() here")
-    #     print("Workspace cancelled. Running plan to deploy new resources.")
-    #     print("I would run planWorkspace() here")
-    #     print("Workspace plan complete. Running apply to deploy new resources.")
-    #     print("I would run applyWorkspace() here")
-    #     print("Workspace apply complete. New resources deployed.")
-
-    # print("Post Action 1: Pull current output for instance_id and write to COS cancellation bucket [future state].")
-    # pullInstanceIdOutput = pullInstanceId()
-    # print("Current instance ID is: " + str(pullInstanceIdOutput))
-    # print("writing to COS bucket")
-    # writeCosFile(instance=pullInstanceIdOutput, cosFile='cancel-queue/' + instance + '-cancel.txt')
-    # print("File written to COS bucket.")
-
 except ApiException as ae:
-    print("Pull of outputs failed.")
+    print("Workspace operation failed.")
     print(" - status code: " + str(ae.code))
     print(" - error message: " + ae.message)
     if ("reason" in ae.http_response.json()):
---
+++ tagfilter/find-by-tag.py
@@ -0,0 +1,62 @@
+import os
+import json
+import sys
+# import SoftLayer
+# from SoftLayer import TicketManager, Client
+from ibm_platform_services import ResourceManagerV2, GlobalSearchV2, GlobalTaggingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_cloud_sdk_core import ApiException
+
+## Construct IAM Authentication using IBMCLOUD_API_KEY Environment variable
+authenticator = IAMAuthenticator(os.environ.get('IBMCLOUD_API_KEY'))
+
+def tagService():
+    tagService = GlobalTaggingV1(authenticator=authenticator)
+    tagService.set_service_url('https://tags.global-search-tagging.cloud.ibm.com')
+    return tagService
+
+def searchService():
+    searchService = GlobalSearchV2(authenticator=authenticator)
+    searchService.set_service_url('https://api.global-search-tagging.cloud.ibm.com')
+    return searchService
+
+def classicIaasTags():
+    client = tagService()
+    tag_list = client.list_tags(
+        tag_type='user',
+        attached_only=True,
+        full_data=True,
+        providers=['ims'],
+        order_by_name='asc').get_result()
+
+    print(json.dumps(tag_list, indent=2))
+
+def getServiceInstances():
+    client = searchService()
+    tagQuery = '(family:ims) AND (tags:"project:rolling-iaas")'
+    result = client.search(
+        query=tagQuery,
+        fields=["*"]
+    ).get_result()
+    resources = result['items']
+    print(str(len(resources)) + " bare metal instances match your search:")
+    for resource in resources:
+        print(resource['doc']['id'])
+
+# def slCient():
+#     client = SoftLayer.create_client_from_env(
+#         username=os.environ.get('IAAS_CLASSIC_USERNAME'),
+#         api_key=os.environ.get('IAAS_CLASSIC_API_KEY')
+#     )
+#     return client
+
+# def getOpenCancelTicket(hardwareId):
+#     client = slCient()
+#     getOpenTicket = client['Hardware_Server'].getOpenCancellationTicket(id=hardwareId)
+#     ticketId = getOpenTicket['id']
+#     return ticketId
+
+try:
+    getServiceInstances()
+except ApiException as ex:
+    print("Method failed with status code " + str(ex.code) + ": " + ex.message)
