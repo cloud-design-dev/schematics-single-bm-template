c58eea502bab653742ad53c12a7003652d8346c8
greyhoundforty <ryantiffany@fastmail.com>
2023-04-10 09:23:11 -0500

    Updated to include sl module for pip

--- containers/clean-up/tagfilter/find-by-tag.py
+++ containers/clean-up/tagfilter/find-by-tag.py
@@ -31,23 +31,34 @@ def classicIaasTags():
 
     print(json.dumps(tag_list, indent=2))
 
+
+def slClient():
+    client = SoftLayer.create_client_from_env(
+        username='apikey',
+        api_key=os.environ.get('IBMCLOUD_API_KEY')
+    )
+    return client
+
 def getServiceInstances():
     client = searchService()
-    tagQuery = '(family:ims) AND (tags:"reclaim_immediately")'
+    classicClient = slClient()
+    tagQuery = '(family:ims) AND (tags:"project:rolling-iaas")'
     result = client.search(
         query=tagQuery,
         fields=["*"]
     ).get_result()
     resources = result['items']
+    
+    instances_to_update = []
+    for resource in resources:
+        # Check if the instance has an open cancellation ticket
+        instance_id = resource['resource_id']
+        open_ticket_id = getOpenTicket(instance_id)
+        if open_ticket_id is not None:
+            instances_to_update.append(resource)
+    
+    return instances_to_update
 
-    return list(resources)
-
-def slClient():
-    client = SoftLayer.create_client_from_env(
-        username=os.environ.get('IAAS_CLASSIC_USERNAME'),
-        api_key=os.environ.get('IAAS_CLASSIC_API_KEY')
-    )
-    return client
 
 ## failing if the instance does not have an open cancellation ticket. In this case we have 1 machine that does not have an open cancellation ## ticket
 
@@ -55,17 +66,31 @@ def getInstanceIds():
     client = slClient()
     svcs = getServiceInstances()
     for svc in svcs:
-        openTicketId = getOpenTicket(instanceId=svc['doc']['id'])
+        openTicketId = client['Hardware_Server'].getOpenCancellationTicket(id=svc)
         getTicketUpdates = client['Ticket'].getUpdates(id=openTicketId)
         print(getTicketUpdates)
 
+# def getOpenTicket(instanceId):
+#     client = slClient()
+#     openTicketId = client['Hardware_Server'].getOpenCancellationTicket(id=instanceId)
+#     ticketId = openTicketId['id']
+#     return str(ticketId) if ticketId else print()
+
 def getOpenTicket(instanceId):
     client = slClient()
-    openTicketId = client['Hardware_Server'].getOpenCancellationTicket(id=instanceId)
-    ticketId = openTicketId['id']
-    return str(ticketId) if ticketId else 'null'
+    tickets = client['Hardware_Server'].getActiveTickets(filter={
+        'hardwareId': {'operation': instanceId},
+        'status': {'operation': 'open'},
+        'title': {'operation': 'Cancellation Request'}
+    })
+    if tickets:
+        return tickets[0]['id']
+    else:
+        return None
 
 try:
-    getInstanceIds()
+    instances = getServiceInstances()
+    # for instance in instances:
+    #     print(instance['resource_id'])
 except ApiException as ex:
     print("Method failed with status code " + str(ex.code) + ": " + ex.message)
