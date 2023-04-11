d35bc458ed48943789c2839287bc07c02fb25d12
greyhoundforty <ryantiffany@fastmail.com>
2023-04-11 09:57:28 -0500

    Adding in testing job to verify private endpoints

---
+++ containers/testing-env/main.py
@@ -0,0 +1,78 @@
+import os
+import logging
+from logdna import LogDNAHandler
+import time
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_cloud_sdk_core import ApiException
+from ibm_schematics.schematics_v1 import SchematicsV1
+from datetime import datetime
+from ibm_platform_services import GlobalTaggingV1
+import SoftLayer
+from SoftLayer import VSManager, Client
+
+
+
+# Set up IAM authenticator and pull refresh token
+authenticator = IAMAuthenticator(
+    apikey=os.environ.get('IBMCLOUD_API_KEY'),
+    client_id='bx',
+    client_secret='bx'
+    )
+
+refreshToken = authenticator.token_manager.request_token()['refresh_token']
+
+def logDnaLogger():
+    key = os.environ.get('LOGDNA_INGESTION_KEY')
+    log = logging.getLogger('logdna')
+    log.setLevel(logging.INFO)
+
+    options = {
+        'index_meta': True,
+        'tags': 'testing-ce-private-endpoints',
+        'url': 'https://logs.private.us-south.logging.cloud.ibm.com/logs/ingest',
+        'log_error_response': True
+    }
+
+    logger = LogDNAHandler(key, options)
+
+    log.addHandler(logger)
+
+    return log
+
+def slClient():
+    client = SoftLayer.create_client_from_env(
+        username='apikey',
+        api_key=os.environ.get('IBMCLOUD_API_KEY')
+    )
+    return client
+
+def schematicsClient():
+    client = SchematicsV1(authenticator=authenticator)
+    schematicsURL = 'https://private-us-east.schematics.cloud.ibm.com'
+    client.set_service_url(schematicsURL)
+    return client
+
+def getWorkspaces():
+    client = schematicsClient()
+    workspaces = client.list_workspaces().get_result()
+    for workspace in workspaces:
+        print(workspace)
+
+def getVirtualGuests():
+    client = slClient()
+    virtualGuests = client['Account'].getVirtualGuests()
+    for virtualGuest in virtualGuests:
+        print(virtualGuest)
+
+try:
+    log = logDnaLogger()
+    log.info(":: Attempting list of workspaces using the private endpoint")
+    getWorkspaces()
+    log.info(":: Attempting list of virtual guests using the private endpoint")
+    getVirtualGuests()
+except ApiException as ae:
+    log.error(":: Code failed.")
+    log.error(" - status code: " + str(ae.code))
+    log.error(" - error message: " + ae.message)
+    if ("reason" in ae.http_response.json()):
+        log.error(" - reason: " + ae.http_response.json()["reason"])
